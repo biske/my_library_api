32e1f1178585325ad4293381151bb83f2181d914
Иван Бишевац <ivan.bisevac@gmail.com>
2017-07-14 23:12:44 +0200

    user-registration, adds spec for checking output data from create user endpoint

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -21,12 +21,15 @@ require 'rspec/rails'
 # require only the support files necessary.
 #
 # Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+require File.expand_path('../support/json_helpers', __FILE__)
 
 # Checks for pending migration and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Support::JsonHelpers
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,14 +2,22 @@ require 'rails_helper'
 
 describe 'users' do
   describe 'POST /users' do
+    let(:email) { 'test@test.com' }
     it 'returns status 201' do
-      post '/users', params: { user: { email: 'test@test.com' } }
+      post '/users', params: { user: { email: email } }
       expect(response.status).to eq 201
     end
 
     it 'returns 422 if validation fails' do
       post '/users', params: { user: { email: nil } }
       expect(response.status).to eq 422
+    end
+
+    it 'returns user data' do
+      post '/users', params: { user: { email: email } }
+      
+      expect(json.keys.sort).to eq ['id', 'email', 'created_at', 'updated_at'].sort
+      expect(json['email']).to eq email
     end
   end
 end
