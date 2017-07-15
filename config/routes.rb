58bdc23125f94277d1d242a83a9bf428a7b29c3f
Иван Бишевац <ivan.bisevac@gmail.com>
2017-07-15 11:41:03 +0200

    user-registration, adds devise under v1 scope

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, only: :registrations, controllers: { registrations: 'registrations' }, defaults: { format: :json }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope 'v1', defaults: { format: 'json' } do
+    devise_for :users, only: :registrations, controllers: { registrations: 'registrations' }, defaults: { format: :json }
+  end
 end
--- spec/requests/v1/users_spec.rb
+++ spec/requests/v1/users_spec.rb
@@ -6,17 +6,17 @@ describe 'users' do
     let(:password) { '123456' }
 
     it 'returns status 201' do
-      post '/users', params: { user: { email: email, password: password } }
+      post '/v1/users', params: { user: { email: email, password: password } }
       expect(response.status).to eq 201
     end
 
     it 'returns 422 if validation fails' do
-      post '/users', params: { user: { email: nil, password: password } }
+      post '/v1/users', params: { user: { email: nil, password: password } }
       expect(response.status).to eq 422
     end
 
     it 'returns user data' do
-      post '/users', params: { user: { email: email, password: password } }
+      post '/v1/users', params: { user: { email: email, password: password } }
       
       expect(json.keys.sort).to eq ['id', 'email', 'created_at', 'updated_at'].sort
       expect(json['email']).to eq email
